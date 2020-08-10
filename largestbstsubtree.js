e6431a37dcdebf61de8373116720c3e85d84491d
kevindai777 <kevin.dai.2451@gmail.com>
2020-08-10 18:06:34 -0400

    solutions

---
+++ largestbstsubtree.js
@@ -0,0 +1,87 @@
+//Objective is, given a binary tree, to find the largest subtree that is
+//also a BST.
+
+class Node {
+    constructor(left, right, val) {
+      this.left = left
+      this.right = right
+      this.val = val
+    }
+}
+  
+class Tree {
+    constructor(root) {
+      this.root = null
+    }
+  
+    createRoot(val) {
+      this.root = new Node(null, null, val)
+    }
+  
+    addLeftNode(node, val) {
+      node.left = new Node(null, null, val)
+    }
+  
+    addRightNode(node, val) {
+      node.right = new Node(null, null, val)
+    }
+}
+
+let tree = new Tree()
+tree.createRoot(1)
+tree.addLeftNode(tree.root, 2)
+tree.addRightNode(tree.root, 5)
+tree.addLeftNode(tree.root.left, 3)
+tree.addRightNode(tree.root.right, 6)
+tree.addRightNode(tree.root.left, 4)
+
+
+//
+
+function largestBSTSubtree(root) {
+    if (!root) {
+        return 0
+    }
+    
+    if (!root.left && !root.right) {
+        return 1
+    }
+    
+    //If we found a valid BST subtree
+    if (isValid(root, null, null)) {
+        return count(root)
+    }
+    
+    function isValid(root, min, max) {
+        if (!root) {
+            return true
+        }
+        
+        if (min != null && min >= root.val) {
+            return false
+        }
+        
+        if (max != null && max <= root.val) {
+            return false
+        } 
+        
+        return isValid(root.left, min, root.val) && isValid(root.right, root.val, max)
+    }
+    
+    //Find the number of nodes in the subtree
+    function count(root) {
+        if (!root) {
+            return 0
+        }
+        
+        if (!root.left && !root.right) {
+            return 1
+        }
+        
+        return count(root.left) + count(root.right) + 1
+    }
+    
+    //Find the largest subtree in the left and right subtrees
+    return Math.max(largestBSTSubtree(root.left), largestBSTSubtree(root.right))
+}
+largestBSTSubtree(tree.root)
